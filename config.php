b871a782b6a371cfa14aeaaa98e8f562a79bab86
Yasin Rabiee <hlww6303@gmail.com>
2025-04-23 15:18:43 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,10 @@
 <?php
 	define('dbServername', 'localhost');
-<<<<<<< HEAD
 	define('dbUsername', 'yasinrabiee_shortner');
 	define('dbPassword', 'Yasinrb021');
 	define('dbName', 'yasinrabiee_shortner');
 	// Replace url
 	define('url', 'https://yasinrabiee.ir/url/?url=');
-=======
 	define('dbUsername', 'shortner');
 	define('dbPassword', 'Yasinrb021');
 	define('dbName', 'shortner');
@@ -18,7 +16,6 @@
 
 	$conn = mysqli_connect(dbServername, dbUsername, dbPassword, dbName);
 	mysqli_set_charset($conn, 'utf8mb4');
->>>>>>> a4615c88a7f61f26dbb32aae42171b30aee76ad7
 	
 	define('adminUsername', 'admin');
 	define('adminPass', 'root');
